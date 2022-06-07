381f656bc3d16eb83f05d3f6f0ce7498b68b5595
MagicSoft <magicsoft.uz@gmail.com>
2022-06-07 11:21:38 +0500

    add Enhancer monitor

--- src/components/store/store.js
+++ src/components/store/store.js
@@ -6,4 +6,15 @@ const reducers = combineReducers({
   songs: songReducer,
 });
 
-export const store = createStore(reducers);
+const monitorEnhacer = (storage) => (reducerEnhacer, intialState, enhancer) => {
+  const monitorReducer = (state, action) => {
+    const start = performance.now();
+    const newReducer = (state, action);
+    const end = performance.now();
+    const timing = end - start;
+    return newReducer;
+  };
+  return storage(monitorReducer, intialState, enhancer);
+};
+
+export const store = createStore(reducers, {}, monitorEnhacer);
